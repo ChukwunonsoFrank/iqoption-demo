7c9fbfe6d72a173632320e83650ebc9fc211a1bb
Chukwunonso Frank <franknonso114@gmail.com>
2025-05-25 00:03:54 +0100

    ui updates on robot page

--- resources/views/livewire/dashboard/robot.blade.php
+++ resources/views/livewire/dashboard/robot.blade.php
@@ -9,7 +9,7 @@
                 <div class="mb-4">
                     <label for="input-label" class="block text-xs font-medium mb-2 text-zinc-300">Amount</label>
                     <div class="relative">
-                        <input type="email"
+                        <input type="text"
                             class="bg-navbar text-white border border-gray-700 text-sm peer py-2.5 sm:py-3 px-4 ps-11 block w-full rounded-lg sm:text-sm focus:outline-0"
                             placeholder="">
                         <div
@@ -21,21 +21,70 @@
 
                 <div class="mb-4">
                     <label for="input-label" class="block text-xs font-medium mb-2 text-zinc-300">Account</label>
-                    <select
-                        class="bg-navbar text-white border border-gray-700 text-sm py-3 px-4 pe-9 block w-full rounded-lg focus:outline-0">
-                        <option>Demo account</option>
-                        <option>Real account</option>
-                    </select>
+                    <div class="flex-1 md:flex-none relative">
+                        <div x-on:click="toggleTradingAccountSelect()"
+                            class="flex items-center space-x-3 py-2.5 sm:py-3 px-4 border border-gray-700 bg-navbar rounded-lg text-[#FFFFFF]">
+                            <div class="flex-1">
+                                <p class="text-sm">Demo Account</p>
+                                {{-- <template x-if="!selectedAccountBoolean">
+                                    <p class="text-sm">Demo Account</p>
+                                </template>
+                                <template x-if="selectedAccountBoolean">
+                                    <p class="text-sm">Real Account</p>
+                                </template> --}}
+                            </div>
+                            <div class="flex-none justify-self-end">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14"
+                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round"
+                                    class="lucide lucide-chevron-down-icon lucide-chevron-down">
+                                    <path d="m6 9 6 6 6-6" />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="relative">
+                        <div x-cloak x-show="isTradingAccountSelectOpen"
+                            @click.outside="isTradingAccountSelectOpen = false"
+                            class="border-gray-700 bg-navbar absolute border rounded-lg w-full h-24 overflow-scroll z-10 p-2 mt-1">
+                            <div x-on:click="isTradingAccountSelectOpen = false"
+                                class="hover:bg-[#38394f] cursor-pointer flex items-center space-x-3 px-4 py-2 rounded-md text-[#FFFFFF]">
+                                <div class="flex-1">
+                                    <p class="text-sm">Demo Account - $400</p>
+                                </div>
+                            </div>
+                            <div x-on:click="isTradingAccountSelectOpen = false"
+                                class="hover:bg-[#38394f] cursor-pointer flex items-center space-x-3 px-4 py-2 rounded-md text-[#FFFFFF]">
+                                <div class="flex-1">
+                                    <p class="text-sm">Live Account - $400</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-                <div class="mb-4">
-                    <label for="input-label" class="block text-xs font-medium mb-2 text-zinc-300">Duration</label>
-                    <select
-                        class="bg-navbar text-white border border-gray-700 text-sm py-3 px-4 pe-9 block w-full rounded-lg focus:outline-0">
-                        <option>5 mins</option>
-                        <option>10 min</option>
-                        <option>15 min</option>
-                    </select>
+                <div class="mb-4 flex items-center space-x-2">
+                    <div class="flex-1">
+                        <label for="input-label" class="block text-xs font-medium mb-2 text-zinc-300">Duration</label>
+                        <input type="email" value="24 hours"
+                            class="bg-navbar text-white text-start border border-gray-700 text-sm py-2.5 sm:py-3 px-4 block w-full rounded-lg sm:text-sm focus:outline-0"
+                            placeholder="" readonly>
+                    </div>
+                    <div class="flex-1">
+                        <label for="input-label" class="block text-xs font-medium mb-2 text-zinc-300">Exchange</label>
+                        <div
+                            class="w-full text-sm self-center text-center border border-gray-700 py-2.5 sm:py-3 px-4 bg-navbar rounded-md text-[#FFFFFF] focus:outline-0">
+                            <img class="inline" src="{{ asset('assets/icons/binance-logo.svg') }}" alt="binance-logo">
+                        </div>
+                    </div>
+                    <div class="flex-1">
+                        <label for="input-label" class="block text-xs font-medium mb-2 text-zinc-300">Broker</label>
+                        <div
+                            class="w-full text-sm self-center text-center border border-gray-700 py-2.5 sm:py-3 px-4 bg-navbar rounded-md text-[#FFFFFF] focus:outline-0">
+                            <img class="inline" src="{{ asset('assets/icons/fxpro.svg') }}" alt="fxpro-logo">
+                        </div>
+                    </div>
                 </div>
 
                 <div class="mb-5">
@@ -69,9 +118,9 @@
                                     in <span>24</span>hrs
                                 </p>
                                 <p class="text-[10px] text-zinc-300">
-                                    <svg class="inline" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round"
+                                    <svg class="inline" xmlns="http://www.w3.org/2000/svg" width="16"
+                                        height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                                         class="lucide lucide-circle-dollar-sign-icon lucide-circle-dollar-sign">
                                         <circle cx="12" cy="12" r="10" />
                                         <path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8" />
@@ -87,6 +136,23 @@
                                     class="lucide lucide-chevron-down-icon lucide-chevron-down">
                                     <path d="m6 9 6 6 6-6" />
                                 </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="text-sm text-white bg-navbar rounded-lg p-4 border-[0.1px] border-gray-700 mb-5"
+                    role="alert" tabindex="-1" aria-labelledby="hs-with-description-label">
+                    <div class="flex">
+                        <div class="shrink-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-badge-info-icon lucide-badge-info"><path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/><line x1="12" x2="12" y1="16" y2="12"/><line x1="12" x2="12.01" y1="8" y2="8"/></svg>
+                        </div>
+                        <div class="ms-2">
+                            <h3 id="hs-with-description-label" class="text-sm font-semibold">
+                                Profit Cycle
+                            </h3>
+                            <div class="mt-1 text-[11px] text-zinc-300">
+                                Profits are accumulated by the robot every 5 minutes.
                             </div>
                         </div>
                     </div>
@@ -296,6 +362,10 @@
 
             toggleStrategyListOverlay() {
                 this.isStrategyListOverlayOpen = !this.isStrategyListOverlayOpen
+            },
+
+            toggleTradingAccountSelect() {
+                this.isTradingAccountSelectOpen = !this.isTradingAccountSelectOpen
             }
         }))
     })
