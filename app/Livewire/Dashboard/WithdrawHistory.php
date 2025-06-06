ebea7d54264a7667a63897927356e7ab43767a52
Chukwunonso Frank <franknonso114@gmail.com>
2025-06-06 08:01:09 +0100

    implement withdrawal history page

--- app/Livewire/Dashboard/WithdrawHistory.php
+++ app/Livewire/Dashboard/WithdrawHistory.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Dashboard;
 
+use App\Models\Withdrawal;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
@@ -9,16 +10,37 @@ use Livewire\Component;
 
 class WithdrawHistory extends Component
 {
+    public $perPage = 10;
+
+    public $visibleCount;
+
+    public $totalWithdrawals;
+
     public function mount()
     {
         if (session()->has('message')) {
             $message = session()->get('message');
             $this->dispatch('withdrawal-created', message: $message)->self();
         }
+
+        $this->totalWithdrawals = Withdrawal::where('user_id', auth()->user()->id)->count();
+        $this->visibleCount = min($this->perPage, $this->totalWithdrawals);
+    }
+
+    public function loadMore(): void
+    {
+        $this->visibleCount = min($this->visibleCount + $this->perPage, $this->totalWithdrawals);
     }
 
     public function render()
     {
-        return view('livewire.dashboard.withdraw-history');
+        $withdrawals = Withdrawal::where('user_id', auth()->user()->id)->latest()->take($this->visibleCount)->get();
+
+        $showLoadMoreButton = $this->visibleCount < $this->totalWithdrawals;
+
+        return view('livewire.dashboard.withdraw-history', [
+            'withdrawals' => $withdrawals,
+            'showLoadMoreButton' => $showLoadMoreButton,
+        ]);
     }
 }
