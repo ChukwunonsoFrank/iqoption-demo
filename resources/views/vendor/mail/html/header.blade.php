e4e3c614f1b1b9ec5ed5aee6392220d7c3d7c89f
Chukwunonso Frank <franknonso114@gmail.com>
2025-06-20 19:42:21 +0100

    wip

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Bullfex')
-<img style="width: 100%" src="https://lin-chain.com/assets/logo.png" class="logo" alt="Bullfex Logo">
+<img src="https://lin-chain.com/assets/logo.png" class="logo" alt="Bullfex Logo">
 @else
 {{ $slot }}
 @endif
